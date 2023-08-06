dc293fdebb6511d824a88d16488d8df0018f73d4
scynck <rafael.mjas@gmail.com>
2023-08-06 15:43:47 -0300

    Aula 17

---
+++ src/main/java/com/example/AppEleitor.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import java.time.LocalDate;
+
+public class AppEleitor {
+
+    public static void main(String[] args) {
+        Cidadao cidadao = new Cidadao();
+        cidadao.setDataNascimento(LocalDate.of(1980, 10, 1));
+        System.out.println(cidadao.idade());
+        System.out.println(cidadao.eleitor());
+    }
+    
+}
---
+++ src/main/java/com/example/Cidadao.java
@@ -0,0 +1,36 @@
+package com.example;
+import java.time.LocalDate;
+
+public class Cidadao {
+
+    private LocalDate dataNascimento;
+    private int idade;
+
+    public void setDataNascimento(LocalDate dataNascimento){
+        this.dataNascimento = dataNascimento;
+    }
+
+    public LocalDate getDataNascimento(){
+        return dataNascimento;
+    }
+
+    public int idade(){
+        this.idade = LocalDate.now().getYear() - dataNascimento.getYear();
+        return idade;
+    }
+
+    public String eleitor(){
+        /*IMPORTANTE
+         * Quando se tem o return dentro de um encadeamento de ifs, não é necessário utilizar o else if, porque se ele adentrar um dos ifs, 
+         * ao passar pelo return, ele já sai da função ou do método
+         */
+        if(idade < 16)
+            return "Não é eleitor";
+        
+        if(idade > 16 && idade < 18 || idade > 70)
+            return "Eleitor facultativo";
+        
+        else return "Eleitor obrigatório";
+            
+        }
+    }
